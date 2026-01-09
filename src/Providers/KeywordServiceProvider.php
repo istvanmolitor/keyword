6528e4a202ac0cd5e390482ecd1008c32a133ee1
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-01-09 13:09:50 +0100

    init

---
+++ src/Providers/KeywordServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Molitor\Keyword\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class KeywordServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
